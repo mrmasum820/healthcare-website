154116ea2fb6dffc1ffc42bd8ae8e3d6d569359e
mrmasum820 <mrmasum820@gmail.com>
2021-10-19 19:00:16 +0600

    firebase authentication added

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,11 +1,11 @@
 // Web App's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyByKz_tPsWPfzHk_vIxJSIHz9vC0v_Fl6g",
-    authDomain: "medicit-health-care.firebaseapp.com",
-    projectId: "medicit-health-care",
-    storageBucket: "medicit-health-care.appspot.com",
-    messagingSenderId: "1091188732087",
-    appId: "1:1091188732087:web:aff5639be8dd1d98ef5d32"
+    apiKey: "AIzaSyDyFwza4pw1qP8Nd8TFJoASeO8fmZOGC3A",
+    authDomain: "healthcare-website-2a686.firebaseapp.com",
+    projectId: "healthcare-website-2a686",
+    storageBucket: "healthcare-website-2a686.appspot.com",
+    messagingSenderId: "91209050087",
+    appId: "1:91209050087:web:b0bd14e68bf2bcb339bf42"
 };
 
 export default firebaseConfig;
