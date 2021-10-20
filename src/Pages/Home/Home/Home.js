d4f2c1bb171991ac62b3fb93549a3308ebf9388b
mrmasum820 <mrmasum820@gmail.com>
2021-10-20 21:25:04 +0600

    home two section added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import IntensiveUnit from '../IntensiveUnit/IntensiveUnit';
 import Services from '../Services/Services';
+import SurpriseBody from '../SurpriseBody/SurpriseBody';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <SurpriseBody></SurpriseBody>
             <Services></Services>
+            <IntensiveUnit></IntensiveUnit>
         </div>
     );
 };
